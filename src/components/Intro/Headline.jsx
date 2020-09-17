1e900154236c2b5eb5a305518d78fc0fa8f1a65b
jonmccon <jonmccon@gmail.com>
2020-09-16 17:12:12 -0700

    added in 2 episode player at top

--- src/components/Intro/Headline.jsx
+++ src/components/Intro/Headline.jsx
@@ -13,8 +13,10 @@ class Headline extends Component {
           <h1>{headline}</h1>
         </div>
 
-        <iframe id="podcast-one" width="100%" height="180" frameborder="no" scrolling="no" seamless src="https://share.transistor.fm/e/82eb0d20"></iframe>
-        <iframe id="podcast-two" width="100%" height="180" frameborder="no" scrolling="no" seamless src="https://share.transistor.fm/e/2b18b97a"></iframe>
+        
+        <iframe id="podcast-one" height="200px" width="100%" frameborder="no" scrolling="no" seamless src="https://player.simplecast.com/1c1d24a9-1967-4e16-ae31-77bd11d5f011?dark=false"></iframe>
+        <iframe id="podcast-two" height="200px" width="100%" frameborder="no" scrolling="no" seamless src="https://player.simplecast.com/cbc0fabd-0d1c-4bc1-887a-5786d2d4575e?dark=false"></iframe>
+        <iframe id="podcast-three" height="200px" width="100%" frameborder="no" scrolling="no" seamless src="https://player.simplecast.com/cbc0fabd-0d1c-4bc1-887a-5786d2d4575e?dark=false"></iframe>
 
         {/* When you merge this into test, give a name to the form element instead of the derivative styling */}
         <div className="headline-contact">
