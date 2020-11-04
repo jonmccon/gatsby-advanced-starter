188362b9692808854df2f4afb81cb6b43daa3488
jonmccon <jonmccon@gmail.com>
2020-11-03 18:23:37 -0800

    description tweak

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -5,7 +5,7 @@ const config = {
   siteLogo: "/scShareBanner.png", // Logo used for SEO and manifest.
   siteUrl: "https://seattlecreative.directory", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription: "Phonebook for Seattle design community", // Website description used for RSS feeds/meta description tag.
+  siteDescription: "Phonebook for the Seattle design community", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteRssTitle: "Seattle Creative Directory Rss feed", // Title of the RSS feed
   // siteFBAppID: "1825356251115265", // FB Application ID for using app insights
