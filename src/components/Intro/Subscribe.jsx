3689a23c10628daf5ce7f7c0d26f25d3ec0f9fb7
jonmccon <jonmccon@gmail.com>
2021-03-12 16:22:00 -0800

    asasasas

--- src/components/Intro/Subscribe.jsx
+++ src/components/Intro/Subscribe.jsx
@@ -6,22 +6,22 @@ import React from "react"
     return (
       
 
-      <form data-netlify="true" name="subscribe" class="email-form">
+      <form data-netlify="true" name="contact" data-netlify-honeypot="bot-field" action="subscribed" className="email-form">
         
-        {/* data-netlify-honeypot="bot-field" action="subscribed" */}
-        {/* <input
-          class="hidden"
-          name="bot-field"
-          placeholder="Don't fill this out if you're a human"
-        /> */}
+        
         <input
-          class="email-form form-control valid"
+          className="email-form form-control valid"
           name="email"
           id="email"
           type="email"
           onfocus="this.placeholder = ''"
           onblur="this.placeholder = 'Emails'"
           placeholder="Emails"
+        />
+        <input
+          className="hidden"
+          name="bot-field"
+          placeholder="Don't fill this out if you're a human"
         />
         
         <button type="submit">
