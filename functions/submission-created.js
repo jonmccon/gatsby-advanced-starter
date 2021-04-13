94205552b2e5fe0263400beba2a35f34e69a3517
jonmccon <jonmccon@gmail.com>
2021-04-13 16:56:29 -0700

    changed to import instead

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,5 +1,6 @@
 // require('dotenv').config()
-const fetch = require('node-fetch')
+// const fetch = require('node-fetch')
+import fetch from "node-fetch"
 const { EMAIL_TOKEN } = process.env
 exports.handler = async event => {
   const email = JSON.parse(event.body).payload.email
