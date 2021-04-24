1c5a4c6f52a653a8bfb325f7ba32390fd852703d
jonmccon <jonmccon@gmail.com>
2021-04-23 17:53:18 -0700

    form ismostly styled and done, just needs to be oricessed

--- src/components/Submit/Submit.jsx
+++ src/components/Submit/Submit.jsx
@@ -74,158 +74,161 @@ import React from "react"
           id="submit-neighborhood"
           type="text"
           placeholder="Neighborhood"
+          required
         />
         </div>
 
-        <div className="submit-select">
         <p>How many people work with you?</p>
-        
-            <label htmlFor="submit-size">1-4</label>
+        <div className="submit-size">        
             <input
                 className="submit-form form-control valid"
                 name="submit-size"
-                id="1-4"
+                id="sizeSmall"
                 value="1-4"
                 type="radio"
+                required
             />
+            <label htmlFor="sizeSmall">1-4</label>
             
-            <label htmlFor="submit-size">11-40</label> 
             <input
                 className="submit-form form-control valid"
                 name="submit-size"
-                id="11-40"
+                id="sizeMedium"
                 value="11-40"
                 type="radio"
             />
+            <label htmlFor="sizeMedium">11-40</label> 
 
-            <label htmlFor="submit-size">41-100</label> 
             <input
                 className="submit-form form-control valid"
                 name="submit-size"
-                id="41-100"
+                id="sizeLarge"
                 value="41-100"
                 type="radio"
             />
-            
-            <label htmlFor="submit-size">100+</label> 
+            <label htmlFor="sizeLarge">41-100</label> 
+
             <input
                 className="submit-form form-control valid"
                 name="submit-size"
-                id="100+"
+                id="sizeHuge"
                 value="100+"
                 type="radio"
             />
+            <label htmlFor="sizeHuge">100+</label> 
+        </div>
 
         <p>What kinds of work do you do?</p>
+        <div className="submit-select">
         <fieldset>      
             <legend>What kinds of work do you do?</legend>      
             
-            <input type="checkbox" name="favorite_pet" value="3D rendering" id="3D-rendering" />
-            <label htmlFor="3D-rendering">3D rendering</label>
+            <input type="checkbox" name="3Drendering" value="3D rendering" id="3Drendering" />
+            <label htmlFor="3Drendering">3D rendering</label>
             
-            <input type="checkbox" name="favorite_pet" value="advertising" id="advertising" />
+            <input type="checkbox" name="advertising" value="advertising" id="advertising" />
             <label htmlFor="advertising">advertising</label>
 
-            <input type="checkbox" name="favorite_pet" value="architecture" id="architecture" />
+            <input type="checkbox" name="architecture" value="architecture" id="architecture" />
             <label htmlFor="architecture">architecture</label>
 
-            <input type="checkbox" name="favorite_pet" value="branding" id="branding" />
+            <input type="checkbox" name="branding" value="branding" id="branding" />
             <label htmlFor="branding">branding</label>
 
-            <input type="checkbox" name="favorite_pet" value="community" id="community" />
+            <input type="checkbox" name="community" value="community" id="community" />
             <label htmlFor="community">community</label>
 
-            <input type="checkbox" name="favorite_pet" value="engineering" id="engineering" />
+            <input type="checkbox" name="engineering" value="engineering" id="engineering" />
             <label htmlFor="engineering">engineering</label>
 
-            <input type="checkbox" name="favorite_pet" value="environmental" id="environmental" />
+            <input type="checkbox" name="environmental" value="environmental" id="environmental" />
             <label htmlFor="environmental">environmental</label>
 
-            <input type="checkbox" name="favorite_pet" value="events" id="events" />
+            <input type="checkbox" name="events" value="events" id="events" />
             <label htmlFor="events">events</label>
 
-            <input type="checkbox" name="favorite_pet" value="exhibition" id="exhibition" />
+            <input type="checkbox" name="exhibition" value="exhibition" id="exhibition" />
             <label htmlFor="exhibition">exhibition</label>
 
-            <input type="checkbox" name="favorite_pet" value="experiential" id="experiential" />
+            <input type="checkbox" name="experiential" value="experiential" id="experiential" />
             <label htmlFor="experiential">experiential</label>
 
-            <input type="checkbox" name="favorite_pet" value="freelance" id="freelance" />
+            <input type="checkbox" name="freelance" value="freelance" id="freelance" />
             <label htmlFor="freelance">freelance</label>
 
-            <input type="checkbox" name="favorite_pet" value="gaming" id="gaming" />
+            <input type="checkbox" name="gaming" value="gaming" id="gaming" />
             <label htmlFor="gaming">gaming</label>
 
-            <input type="checkbox" name="favorite_pet" value="illustration" id="illustration" />
+            <input type="checkbox" name="illustration" value="illustration" id="illustration" />
             <label htmlFor="illustration">illustration</label>
 
-            <input type="checkbox" name="favorite_pet" value="industrial" id="industrial" />
+            <input type="checkbox" name="industrial" value="industrial" id="industrial" />
             <label htmlFor="industrial">industrial</label>
 
-            <input type="checkbox" name="favorite_pet" value="interior" id="interior" />
+            <input type="checkbox" name="interior" value="interior" id="interior" />
             <label htmlFor="interior">interior</label>
 
-            <input type="checkbox" name="favorite_pet" value="marketing" id="marketing" />
+            <input type="checkbox" name="marketing" value="marketing" id="marketing" />
             <label htmlFor="marketing">marketing</label>
 
-            <input type="checkbox" name="favorite_pet" value="motion" id="motion" />
+            <input type="checkbox" name="motion" value="motion" id="motion" />
             <label htmlFor="motion">motion</label>
 
-            <input type="checkbox" name="favorite_pet" value="naming" id="naming" />
+            <input type="checkbox" name="naming" value="naming" id="naming" />
             <label htmlFor="naming">naming</label>
 
-            <input type="checkbox" name="favorite_pet" value="packaging" id="packaging" />
+            <input type="checkbox" name="packaging" value="packaging" id="packaging" />
             <label htmlFor="packaging">packaging</label>
 
-            <input type="checkbox" name="favorite_pet" value="photography" id="photography" />
+            <input type="checkbox" name="photography" value="photography" id="photography" />
             <label htmlFor="photography">photography</label>
 
-            <input type="checkbox" name="favorite_pet" value="presentation" id="presentation" />
+            <input type="checkbox" name="presentation" value="presentation" id="presentation" />
             <label htmlFor="presentation">presentation</label>
 
-            <input type="checkbox" name="favorite_pet" value="press" id="press" />
+            <input type="checkbox" name="press" value="press" id="press" />
             <label htmlFor="press">press</label>
 
-            <input type="checkbox" name="favorite_pet" value="print" id="print" />
+            <input type="checkbox" name="print" value="print" id="print" />
             <label htmlFor="print">print</label>
 
-            <input type="checkbox" name="favorite_pet" value="product" id="product" />
+            <input type="checkbox" name="product" value="product" id="product" />
             <label htmlFor="product">product</label>
 
-            <input type="checkbox" name="favorite_pet" value="public relations" id="public relations" />
-            <label htmlFor="public relations">public relations</label>
+            <input type="checkbox" name="publicRelations" value="public relations" id="publicRelations" />
+            <label htmlFor="publicRelations">pr</label>
 
-            <input type="checkbox" name="favorite_pet" value="research" id="research" />
+            <input type="checkbox" name="research" value="research" id="research" />
             <label htmlFor="research">research</label>
 
-            <input type="checkbox" name="favorite_pet" value="education" id="education" />
+            <input type="checkbox" name="education" value="education" id="education" />
             <label htmlFor="education">education</label>
 
-            <input type="checkbox" name="favorite_pet" value="strategy" id="strategy" />
+            <input type="checkbox" name="strategy" value="strategy" id="strategy" />
             <label htmlFor="strategy">strategy</label>
 
-            <input type="checkbox" name="favorite_pet" value="typography" id="typography" />
+            <input type="checkbox" name="typography" value="typography" id="typography" />
             <label htmlFor="typography">typography</label>
 
-            <input type="checkbox" name="favorite_pet" value="ux-ui" id="ux-ui" />
+            <input type="checkbox" name="ux-ui" value="ux-ui" id="ux-ui" />
             <label htmlFor="ux-ui">dogs</label>
 
-            <input type="checkbox" name="favorite_pet" value="vfx" id="vfx" />
+            <input type="checkbox" name="vfx" value="vfx" id="vfx" />
             <label htmlFor="vfx">vfx</label>
 
-            <input type="checkbox" name="favorite_pet" value="video" id="video" />
+            <input type="checkbox" name="video" value="video" id="video" />
             <label htmlFor="video">video</label>
 
-            <input type="checkbox" name="favorite_pet" value="sound" id="sound" />
+            <input type="checkbox" name="sound" value="sound" id="sound" />
             <label htmlFor="sound">sound</label>
 
-            <input type="checkbox" name="favorite_pet" value="voice" id="voice" />
+            <input type="checkbox" name="voice" value="voice" id="voice" />
             <label htmlFor="voice">voice</label>
 
-            <input type="checkbox" name="favorite_pet" value="virtual-reality" id="virtual-reality" />
+            <input type="checkbox" name="virtual-reality" value="virtual-reality" id="virtual-reality" />
             <label htmlFor="virtual-reality">virtual reality</label>
 
-            <input type="checkbox" name="favorite_pet" value="eCommerce" id="eCommerce" />
+            <input type="checkbox" name="eCommerce" value="eCommerce" id="eCommerce" />
             <label htmlFor="eCommerce">eCommerce</label>
 
             <input type="checkbox" name="favorite_pet" value="dogs" id="dogs" />
@@ -234,15 +237,38 @@ import React from "react"
         </fieldset> 
         </div>
 
-        <label htmlFor="submit-notes">Notes:</label> 
-        <input
-          className="submit-form form-control valid"
-          name="submit-notes"
-          id="submit-notes"
-          type="text"
-          placeholder="Any additional types of work, urls, feelings"
-        />
-        
+        <div className="submit-bonus">
+          <p>Would you like to be a guest on the show? Or do you want to hear from someone specifically?</p>
+          <label htmlFor="submit-guest">Would you like to be a guest on the show? Or do you want to hear from someone specifically?</label> 
+          <input
+            className="submit-form form-control valid"
+            name="submit-guest"
+            id="submit-guest"
+            type="text"
+            placeholder="Anyone in the community qualifies to be a part it"
+          />
+
+
+          <p>Would you buy a hardcopy printed directory?</p> 
+          <label htmlFor="submit-print">Would you buy a hardcopy printed directory?</label> 
+          <input
+            className="submit-form form-control valid"
+            name="submit-print"
+            id="submit-print"
+            type="text"
+            placeholder="A beautiful resource or the internet is fine"
+          />
+
+          <p>If you have any questions or suggestions, let us know:</p> 
+          <label htmlFor="submit-notes">If you have any questions or suggestions, let us know:</label> 
+          <input
+            className="submit-form form-control valid"
+            name="submit-notes"
+            id="submit-notes"
+            type="text"
+            placeholder="Any additional types of work, urls, feelings"
+          />
+        </div>        
             
 
         
