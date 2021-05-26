d32fcab1435c53417afeac9c34e1b9f3515f33ef
jonmccon <jonmccon@gmail.com>
2021-05-25 21:24:28 -0700

    show everything function

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -3,58 +3,61 @@ const { EMAIL_TOKEN } = process.env
 const { NOTION_TOKEN } = process.env
 
 // output all form content to netlify console
-// exports.handler = event => {
-// let payload = JSON.parse(event.body).payload;
-// console.log(payload)
+exports.handler = event => {
+let payload = JSON.parse(event.body).payload;
+console.log(payload)
 
-exports.handler = async event => {
+// exports.handler = async event => {
   
-  const submission = JSON.parse(event.body).payload.data
-  const email = JSON.parse(event.body).payload.email
+//   const allData = JSON.parse(event.body).payload.data
+//   const email = JSON.parse(event.body).payload.email
+//   const submission = JSON.parse(event.body).payload.submission
 
-  console.log(`Recieved a submission: ${submission}`)
 
-  // look at the form name and determine which form it came from
-  // if subscribe then payload.email
-  // do the email processing and exit
+//   console.log(`Everything: ${allData}`)
 
-  // if submit then payload.submit
-  // do submit processing and exit
 
-  if (submission.form_name == 'Subscribe - Buttondown') {
-      return fetch('https://api.buttondown.email/v1/subscribers', {
-        method: 'POST',
-        headers: {
-          Authorization: `Token ${EMAIL_TOKEN}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email,"tags":["embeddedFormNetlify"] }), 
-      })
-        .then(response => response.json())
-        .then(data => {
-          console.log(`Submitted to Buttondown:\n ${data}`)
-        })
-        .catch(error => ({ statusCode: 422, body: String(error) }))
+//   // look at the form name and determine which form it came from
+//   // if subscribe then payload.email
+//   // do the email processing and exit
+
+//   // if submit then payload.submit
+//   // do submit processing and exit
+
+//   if (submission.form_name == 'Subscribe - Buttondown') {
+//       return fetch('https://api.buttondown.email/v1/subscribers', {
+//         method: 'POST',
+//         headers: {
+//           Authorization: `Token ${EMAIL_TOKEN}`,
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ email,"tags":["embeddedFormNetlify"] }), 
+//       })
+//         .then(response => response.json())
+//         .then(data => {
+//           console.log(`Submitted to Buttondown:\n ${data}`)
+//         })
+//         .catch(error => ({ statusCode: 422, body: String(error) }))
     
-      } else if (submission.form_name == 'Submit - Notion') {
-        return fetch('https://api.notion.com/v1/pages', {
-        method: 'POST',
-        headers: {
-          Authorization: `Token ${NOTION_TOKEN}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ submission }), 
+//       } else if (submission.form_name == 'Submit - Notion') {
+//         return fetch('https://api.notion.com/v1/pages', {
+//         method: 'POST',
+//         headers: {
+//           Authorization: `Token ${NOTION_TOKEN}`,
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ submission }), 
         
-      })
-        .then(response => response.json())
-        .then(data => {
-          console.log(`Submitted to Notion:\n ${data}`)
-        })
-        .catch(error => ({ statusCode: 422, body: String(error) }))
+//       })
+//         .then(response => response.json())
+//         .then(data => {
+//           console.log(`Submitted to Notion:\n ${data}`)
+//         })
+//         .catch(error => ({ statusCode: 422, body: String(error) }))
 
-    } else {
-      // erroroororo
-    }
+//     } else {
+//       // erroroororo
+//     }
 
   
 }
