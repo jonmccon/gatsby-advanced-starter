4af097dda490369b6f45a73f0a519592625df265
jonmccon <jonmccon@gmail.com>
2021-05-27 08:39:54 -0700

    save the log tis tim dummy!

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,78 +1,79 @@
 import fetch from "node-fetch"
 const { EMAIL_TOKEN } = process.env
 const { NOTION_TOKEN } = process.env
+const { NOTION_DB_ID } = process.env
 
 // output all form content to netlify console
-// exports.handler = event => {
-// let payload = JSON.parse(event.body).payload;
-// console.log(payload)
-
-exports.handler = async event => {
-  
-  const allData = JSON.parse(event.body).payload.data
-  const formName = JSON.parse(event.body).payload.form_name
-  const email = JSON.parse(event.body).payload.email
-
-
-  console.log(`everything: ${allData}`)
-  console.log(`form name: ${formName}`)
-
-
-
-  // look at the form name and determine which form it came from
-  // if subscribe then payload.email
-  // do the email processing and exit
-
-  // if submit then payload.submit
-  // do submit processing and exit
-
-  if (formName == 'Subscribe - Buttondown') {
-      return fetch('https://api.buttondown.email/v1/subscribers', {
-        method: 'POST',
-        headers: {
-          Authorization: `Token ${EMAIL_TOKEN}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email,"tags":["embeddedFormNetlify"] }), 
-      })
-        .then(response => response.json())
-        .then(data => {
-          console.log(`Submitted to Buttondown:\n ${data}`)
-        })
-        .catch(error => ({ statusCode: 422, body: String(error) }))
-    
-      } else if (formName == 'Submit - Notion') {
-        return fetch('https://api.notion.com/v1/pages', {
-        method: 'POST',
-        headers: {
-          Authorization: `Token ${NOTION_TOKEN}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ allData }), 
-        
-      })
-        .then(response => response.json())
-        .then(data => {
-          console.log(`Submitted to Notion:\n ${data}`)
-        })
-        .catch(error => ({ statusCode: 422, body: String(error) }))
-
-    } else {
-      return fetch('https://api.notion.com/v1/pages', {
-        method: 'POST',
-        headers: {
-          Authorization: `Token ${NOTION_TOKEN}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ allData }), 
-        
-      })
-        .then(response => response.json())
-        .then(data => {
-          console.log(`Submitted to Notion:\n ${data}`)
-        })
-        .catch(error => ({ statusCode: 422, body: String(error) }))
-    }
-
-  
+exports.handler = event => {
+let payload = JSON.parse(event.body).payload;
+console.log(payload)
 }
+
+// exports.handler = async event => {
+  
+//   const allData = JSON.parse(event.body).payload.data
+//   const formName = JSON.parse(event.body).payload.form_name
+//   const email = JSON.parse(event.body).payload.email
+
+
+//   console.log(`everything: ${allData}`)
+//   console.log(`form name: ${formName}`)
+
+
+//   // look at the form name and determine which form it came from
+//   // if subscribe then payload.email
+//   // do the email processing and exit
+
+//   // if submit then payload.submit
+//   // do submit processing and exit
+
+//   if (formName == 'Subscribe - Buttondown') {
+//       return fetch('https://api.buttondown.email/v1/subscribers', {
+//         method: 'POST',
+//         headers: {
+//           Authorization: `Token ${EMAIL_TOKEN}`,
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ email,"tags":["embeddedFormNetlify"] }), 
+//       })
+//         .then(response => response.json())
+//         .then(data => {
+//           console.log(`Submitted to Buttondown:\n ${data}`)
+//         })
+//         .catch(error => ({ statusCode: 422, body: String(error) }))
+    
+//       } else if (formName == 'Submit - Notion') {
+//         return fetch('https://api.notion.com/v1/pages', {
+//         method: 'POST',
+//         headers: {
+//           Authorization: `Token ${NOTION_TOKEN}`,
+//           'Content-Type': 'application/json',
+//         },
+//         body: 
+//         // this syntax is wrong, how do I look at that part of the payload
+//         '{
+//           "parent": { "database_id": "{DATABASE_ID}" },
+//           "properties": {
+//             "Name": {
+//               "title": [
+//                 {
+//                   "text": {
+//                     "content": notionContent
+//                   }
+//                 }
+//               ]
+//             }
+//           }
+//         }'
+        
+//       })
+//         .then(response => response.json())
+//         .then(data => {
+//           console.log(`Submitted to Notion:\n ${data}`)
+//         })
+//         .catch(error => ({ statusCode: 422, body: String(error) }))
+
+//     } 
+
+  
+// }
