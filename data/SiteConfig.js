7f503049ecdccae2538852b04d4bcc43fddbabbf
jonmccon <jonmccon@gmail.com>
2021-10-11 18:39:00 -0700

    rebuild

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -21,7 +21,7 @@ const config = {
   userLocation: "Seattle, Earth", // User location to display in the author segment.
   userAvatar: "/static/avatar.gif", // User avatar to display in the author segment.
   userDescription:
-    "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
+    "PNW", // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
   userLinks: [
     {
