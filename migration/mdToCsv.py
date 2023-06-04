454dba245f49bfea599c96aac6352ccf9e31063f
jonmccon <jonmccon@gmail.com>
2023-06-04 00:30:26 -0700

    roughed out more of migration script

--- migration/mdToCsv.py
+++ migration/mdToCsv.py
@@ -5,10 +5,11 @@ from markdown import Markdown
 # Specify the desired order of headers (optional)
 header_order = ["title", "website", 
                 "category", "size",
-                "city", "neighborhood", "map",
+                "city", "neighborhood",
+                "latitude", "longitude",
                 "twit", "inst", 
-                "tags",
-                "linkA","linkB",
+                "linkATitle", "linkAURL",
+                "linkBTitle", "linkBURL",
                 "pullquote", "episodePromo", "episodePerson",
                 "episodeURL", "color",
                 "published"]
@@ -37,21 +38,49 @@ def parse_metadata(markdown_content):
     md.convert(markdown_content)
     metadata = md.Meta
 
-    # Convert linkA from list to string
-    # metadata["linkA"] = " ".join(metadata.get("linkA", []))
-
     # Check if fields exist, and assign empty string if missing
     fields = header_order
 
     for field in fields:
         if field not in metadata:
             metadata[field] = ""
-    
+
     # Remove "-" character from tags
     if "tags" in metadata:
         metadata["tags"] = [tag.lstrip("- ") for tag in metadata["tags"]]
 
+    # Split place data into latitude and longitude
+    if "place" in metadata and metadata["place"]:
+        place_data = metadata["place"][0]
+        if place_data:
+            latitude, longitude = place_data.strip("[]").split(",")
+            metadata["latitude"] = latitude.strip()
+            metadata["longitude"] = longitude.strip()
+        else:
+            metadata["latitude"] = ""
+            metadata["longitude"] = ""
+    else:
+        metadata["latitude"] = ""
+        metadata["longitude"] = ""
+
+   # Extract linkA if available
+    linkA = metadata.get("linkA", [])
+    linkATitle = linkA[0] if linkA and len(linkA) > 0 else ""
+    linkAURL = linkA[1] if linkA and len(linkA) > 1 else ""
+    metadata["linkATitle"] = linkATitle
+    metadata["linkAURL"] = linkAURL
+
+ # Extract linkB if available
+    linkB = metadata.get("linkB")
+    linkBTitle = linkB[0] if linkB and len(linkB) > 0 else ""
+    linkBURL = linkB[1] if linkB and len(linkB) > 1 else ""
+    metadata["linkBTitle"] = linkBTitle
+    metadata["linkBURL"] = linkBURL
+
     return metadata
+
+
+
 
 def write_to_csv(metadata_list, output_file, header_order=None, ignored_fields=None, sort_field=None):
     keys = set().union(*[metadata.keys() for metadata in metadata_list])
@@ -62,6 +91,26 @@ def write_to_csv(metadata_list, output_file, header_order=None, ignored_fields=N
     if header_order is not None:
         keys = [key for key in header_order if key in keys]
 
+    # Extract unique tags
+    all_tags = set()
+    for metadata in metadata_list:
+        if "tags" in metadata:
+            all_tags.update(metadata["tags"])
+
+    # Convert set of tags to a list
+    all_tags = list(all_tags)
+
+    # Add tag columns to keys
+    keys.extend(all_tags)
+
+    # Iterate over each metadata and populate the tag columns
+    for metadata in metadata_list:
+        for tag in all_tags:
+            if tag in metadata.get("tags", []):
+                metadata[tag] = True
+            else:
+                metadata[tag] = False
+
     if sort_field is not None:
         metadata_list.sort(key=lambda x: x.get(sort_field, ""))
 
@@ -69,28 +118,29 @@ def write_to_csv(metadata_list, output_file, header_order=None, ignored_fields=N
         writer = csv.DictWriter(file, fieldnames=keys, quoting=csv.QUOTE_MINIMAL)
         writer.writeheader()
 
+        # Write data rows
         for metadata in metadata_list:
             row = {}
             for key in keys:
                 value = metadata.get(key, "")
 
-                 # Handle different data types appropriately
+                # Handle different data types appropriately
                 if isinstance(value, list):
-                    if key == "tags":
-                        value = ", ".join(value[1:])  # Exclude the first comma
-                    else:
-                        value = ", ".join(value)
-                
-                if isinstance(value, tuple):
-                    if key == "LinkA":
-                        value = value[0].join(value[1])
-                    else:
-                        value = value[0]
+                    value = ", ".join(value)
+                elif isinstance(value, tuple):
+                    value = value[0]
+                else:
+                    value = str(value).strip("[]'")
 
                 row[key] = value
 
-            writer.writerow(row)
+            # Assign linkA and linkB values
+            row["linkATitle"] = metadata.get("linkATitle", "")
+            row["linkAURL"] = metadata.get("linkAURL", "")
+            row["linkBTitle"] = metadata.get("linkBTitle", "")
+            row["linkBURL"] = metadata.get("linkBURL", "")
 
+            writer.writerow(row)
 
 
 # Specify the directory containing the Markdown files
